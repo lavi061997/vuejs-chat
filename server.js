f0bdb20a626f17842297694356d2fa0132c2059a
Lakshya Agrawal <lavi061997@gmail.com>
2018-02-11 16:09:17 +0530

    basic functioning done

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var app = require("express")();
-const http = require('http').Server(app);
-
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(request, response){
   response.sendFile(__dirname + "/index.html");
-})
+});
+
+io.sockets.on('connection',function(socket){
+  console.log("client connected");
+  const user = Date.now();
+
+  socket.on('send', function(message){
+    console.log(message);
+      io.sockets.emit('message', message);
+  });
+  io.emit('message', 'user'+ user + 'connected');
+});
 
 
 http.listen(3600, function(){
